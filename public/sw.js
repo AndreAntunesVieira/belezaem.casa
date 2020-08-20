57151982f987f50ef874f7bb25ca2d14a309f417
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-08-20 02:49:08 -0300

    fix start

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'belezaem.casa-1597899333281'
+const CACHE_NAME = 'belezaem.casa-1597902550514'
 const pageUrls = ["/","/agenda"]
 const staticAssets = ["/styles.css","/logo-white-50.png","/whatsapp.png","/fotos/w65xrjq.jpg","/fotos/boca.jpg"]
 const networkOnlyUrls = []
