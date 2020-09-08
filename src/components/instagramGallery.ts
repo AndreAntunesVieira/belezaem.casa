e46e570867ba4f152ad5a33440abad94b1da363c
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-09-08 01:50:24 -0300

    Add script to update instagram

--- src/components/instagramGallery.ts
+++ src/components/instagramGallery.ts
@@ -2,123 +2,123 @@ const InstagramGallery = [
   {
     link: 'https://www.instagram.com/p/B9nDR0qnenq/?igshid=1ijw3dbsshtb8',
     description:
-      '✨Fall In love ✨ A 2 anos atrás ela foi minha modelo no meu TCC, hoje em mais um dia importante ela…',
+      '✨Fall In love ✨ A 2 anos atrás ela foi minha modelo no meu TCC, hoje em mais um dia importante ela e',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/90090474_135998697953071_8495726798749328607_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=SeeKkurZXyEAX9O9TOe&oh=5bfe6d8645f6817767162c95d328058d&oe=5F30CC1D',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/90090474_135998697953071_8495726798749328607_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=tF0FF70LEBIAX9U_nAy&oh=130700e41625b27272ab9d415c8eeae8&oe=5F7FDBFA',
       width: 640,
       height: 800,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B9hZFLWHX4e/?igshid=kk0ahz8dmye3',
-    description:
-      '🍁Morena Iluminada 🍁 Naturalidade e brilho para a maravilhosa @gab_zambranoo 😍 Arraste para ver o… • See 240 photos and videos on their profile.',
+    link: 'https://www.instagram.com/p/B9hZFLWHX4e/?igshid=kk0ahz8dmye3',
+    description: '🍁Morena Iluminada 🍁 Naturalidade e brilho para a maravilhosa @gab_zambranoo 😍 Arraste para ver o ',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/89378093_207950940285766_6945041283720196354_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=2IKqnUbz_joAX-sCKYQ&oh=cbef109f9c2919b495aa791e9c98c8ff&oe=5F312F84',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/89378093_207950940285766_6945041283720196354_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=4iOuhv10tw0AX8i8wfK&oh=ea08325f8e150eedddf224e18d8f5eb2&oe=5F81C9E7',
       width: 640,
       height: 800,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B5-i_H0HSoa/?igshid=1xc6ipbi8g8i2',
-    description: 'Transforme-se na sua melhor versão... tive o prazer de participar da transformação dessa linda…',
+    link: 'https://www.instagram.com/p/B5-i_H0HSoa/?igshid=1xc6ipbi8g8i2',
+    description: 'Transforme-se na sua melhor versão... tive o prazer de participar da transformação dessa linda @baby',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/77272956_580582499392927_4438610704180467147_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=BQ-xzSCdRVAAX-QpoIY&oh=f0590d95e5fe0b2043dff253ab1db20e&oe=5F2E086D',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/77272956_580582499392927_4438610704180467147_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=BAU-fDS5X7cAX-aaOOQ&oh=75a550074f1cab8de366e53bfb48042a&oe=5F80688A',
       width: 640,
       height: 800,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B3NCtLSnKoc/?igshid=1hokvyvhlfwbs',
-    description:
-      '✨ Preta iluminada ✨ 🖤Black Power🖤 Mechas e corte Arraste para ver o antes ➡️ 👊🏽💪🏽🙋🏽 . . . Hair… • See 240 photos and videos on their profile.',
+    link: 'https://www.instagram.com/p/B3NCtLSnKoc/?igshid=1hokvyvhlfwbs',
+    description: '✨ Preta iluminada ✨ 🖤Black Power🖤 Mechas e corte  Arraste para ver o antes ➡️ 👊🏽💪🏽🙋🏽 . . . H',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/70848618_174386203735528_8028183575614822613_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=fN2wATQrZFMAX-VLcAY&oh=a33c5961467c51c2cf77b54483a26025&oe=5F2E56A6',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/70848618_174386203735528_8028183575614822613_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=5ez6GcF6WE4AX_bZmLu&oh=3ffc597da34dacf25a3f9d98089f9197&oe=5F7FB993',
       width: 640,
       height: 640,
     },
   },
   {
-    link: ' https://www.instagram.com/p/ByQnLSpBCDD/?igshid=5hi1laa6g2ir',
-    description:
-      'O cabelo colorido é uma tendência que veio para ficar... não sei vocês, mas estou apaixonada por… • Follow their account to see 240 posts.',
+    link: 'https://www.instagram.com/p/ByQnLSpBCDD/?igshid=5hi1laa6g2ir',
+    description: 'O cabelo colorido é uma tendência que veio para ficar... não sei vocês, mas estou apaixonada por ess',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/61873171_541403483057585_6336975259829052577_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=W9lcNY_IC3AAX-39tiK&oh=579e6808209e70b10980c61de886dc30&oe=5F2DF05E',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/61873171_541403483057585_6336975259829052577_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=7K5MyqaQbNgAX-kmpeM&oh=45598fabacf4c3a08ce44f0143de26d2&oe=5F81AD39',
       width: 640,
       height: 799,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B97Z59EBLax/?igshid=1va8eay92w9n9',
-    description: 'Um homem com sua masculinidade em dia 💇🏽‍♂👨🏽‍🦱🧔🏽🕺🏽 . Quando digo que cabelo é uma forma de…',
+    link: 'https://www.instagram.com/p/B97Z59EBLax/?igshid=1va8eay92w9n9',
+    description: 'Um homem com sua masculinidade em dia 💇🏽‍♂👨🏽‍🦱🧔🏽🕺🏽 . Quando digo que cabelo é uma forma de ',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/90406593_117188123229514_796801784121201283_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=vCAisledC9gAX-2Muq3&oh=a7313510902468bc85437f00ac648e87&oe=5F2DC51C',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/90406593_117188123229514_796801784121201283_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=NmjUhEHKqCsAX9-fqhd&oh=7840bf96e1f98cedbaf7d677e2ec3021&oe=5F825529',
       width: 640,
       height: 800,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B7WP1jQB4fG/?igshid=115qpfbp37l0y',
-    description: 'Vocês estão prontas para o verão ??? A @aline_dorr está... vem verão 🌊☀️🌊 . . . Hair…',
+    link: 'https://www.instagram.com/p/B7WP1jQB4fG/?igshid=115qpfbp37l0y',
+    description: 'Vocês estão prontas para o verão ??? A @aline_dorr  está... vem verão 🌊☀️🌊 . . . Hair @thayoliveir',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/81460255_2571186163201730_851166561103388812_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=ODuZqHmHxnMAX-bLoLU&oh=eea925fe53eef6ea5c920c7e1ce55e78&oe=5F2EF7B0',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/81460255_2571186163201730_851166561103388812_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=JnPcQSDPjs0AX_YktKE&oh=bed8c2a072455d8aff13a3c88ce0fba2&oe=5F817BCB',
       width: 640,
       height: 799,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B9b0LNgHArM/?igshid=1gz8tu4cl77fk',
-    description: '✨ Alongamento de cílios volume brasileiro ✨. Passe pro lado ➡️ e veja a PERFEIÇÃO desse olhar 😍.…',
+    link: 'https://www.instagram.com/p/B9b0LNgHArM/?igshid=1gz8tu4cl77fk',
+    description:
+      '✨ Alongamento de cílios volume brasileiro ✨. Passe pro lado ➡️ e veja a PERFEIÇÃO desse olhar 😍. @t',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/87346400_524285011545065_5311243768890319859_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=By2YkoxhT40AX-DzuV_&oh=a371571b27961d65307d08a0ff9fbf44&oe=5F2E99A1',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/87346400_524285011545065_5311243768890319859_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=aVZXCff2L50AX9jU4kf&oh=da5858c9227d686b1f21d0ff2d2f0a8e&oe=5F80AC46',
       width: 640,
       height: 640,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B7uJlqYhHoZ/?igshid=z5aupqfqkfcn',
-    description: 'Maquiagem ok 👌🏽 . Sextou MARAVILHOSAAAAAA @fernandalorencet 😍💋 . . . . . . . . #makeup #maquiagem…',
+    link: 'https://www.instagram.com/p/B7uJlqYhHoZ/?igshid=z5aupqfqkfcn',
+    description: 'Maquiagem ok 👌🏽 . Sextou MARAVILHOSAAAAAA @fernandalorencet 😍💋 . . . . . . . . #makeup #maquiage',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/82646797_166343568017486_1301344916782784142_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=U_UmU1R7_UQAX9VRxgA&oh=dc450ccb968939aa01f009e6c534590e&oe=5F3182F8',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/82646797_166343568017486_1301344916782784142_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=zUuV0QH2YmwAX-5p3bb&oh=fcdc1d76d68b5db05f19f3e95d168a98&oe=5F828B93',
       width: 640,
       height: 640,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B6iTtsSHmHn/?igshid=1lradr9u1mw6l',
-    description: '✨ Sobrancelhas PERFEITAS✨ . . . . . . . . . #designdesobrancelhas #sobrancelhasperfeitas…',
+    link: 'https://www.instagram.com/p/B6iTtsSHmHn/?igshid=1lradr9u1mw6l',
+    description:
+      '✨ Sobrancelhas PERFEITAS✨ . . . . . . . . . #designdesobrancelhas #sobrancelhasperfeitas #sobrancelh',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/76976817_160994041825882_2672009289801452215_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ZneUK55OUqUAX_O-qFq&oh=1e42fa5a205859ac14b2932ca4d367d3&oe=5F2EFDA5',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/76976817_160994041825882_2672009289801452215_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=rqgrkR5_4P4AX8wSxoR&oh=3c66a6959939ae797ca9c6181f424a57&oe=5F7FE5C2',
       width: 640,
       height: 640,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B1wq0yOB9H4/?igshid=1bzjfvk2xlp9t',
-    description: '#tbt do curso de produção de moda para fotografia, ministrado por @haisahofmann_ e @cavalheirolucas…',
+    link: 'https://www.instagram.com/p/B1wq0yOB9H4/?igshid=1bzjfvk2xlp9t',
+    description: '#tbt do curso de produção de moda para fotografia, ministrado por @haisahofmann_ e @cavalheirolucas ',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/68687956_496865111094237_8795819830208355984_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=-SaeXaqLsw4AX9tjNCm&oh=00a40edce7b2e40e63e8b54de9bb702b&oe=5F2F56C5',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/68687956_496865111094237_8795819830208355984_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=8MHLeCeA0mIAX-DrOWJ&oh=d307c96c72fd307cd163f8922c9b3380&oe=5F826545',
       width: 640,
       height: 640,
     },
   },
   {
-    link: ' https://www.instagram.com/p/B3A1gFjBQTU/?igshid=1sinagaoeccps',
-    description: 'Makeup Rosé ❇️✨ . . . . . . . . #makeup #makeprofissional #makeperfeita #makeincrivel #supermarke…',
+    link: 'https://www.instagram.com/p/B3A1gFjBQTU/?igshid=1sinagaoeccps',
+    description:
+      'Makeup Rosé ❇️✨ . . . . . . . . #makeup #makeprofissional #makeperfeita #makeincrivel #supermarke #m',
     image: {
       src:
-        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/e35/70135309_421076008540997_4664381498302929284_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=IGl8wnhCrSwAX-OPkXS&oh=64deb5d05dd9fea538d28ae05917b2ad&oe=5F302A7D',
+        'https://instagram.fpoa27-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/70135309_421076008540997_4664381498302929284_n.jpg?_nc_ht=instagram.fpoa27-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=jK9KWEPavhcAX--IF8s&oh=1f7bbd4bbb6901e7915ee861a5336cd1&oe=5F7F111A',
       width: 640,
       height: 640,
     },
