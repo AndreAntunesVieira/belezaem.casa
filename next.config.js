34f339ebeae546858f99e4c102ede74772eee91c
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-09-18 22:20:01 -0300

    Fix now config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-  exportTrailingSlash: true,
-  env: {
-    MONGODB_URL: process.env.MONGODB_URL,
-  },
-  serverless: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-    }
-  },
+  trailingSlash: true,
+  serverless: true
 }
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'belezaem.casa-1600478050993'
+const CACHE_NAME = 'belezaem.casa-1600478403784'
 const pageUrls = ["/","/agenda"]
 const staticAssets = ["/styles.css","/logo-white-50.png","/whatsapp.png","/fotos/w65xrjq.jpg","/fotos/boca.jpg"]
 const networkOnlyUrls = []
