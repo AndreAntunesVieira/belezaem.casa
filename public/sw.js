f5512f82a99829d01d74699a80b8b87fe13871fe
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-09-22 01:32:00 -0300

    remove unused route

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'belezaem.casa-1600748620896'
+const CACHE_NAME = 'belezaem.casa-1600749123044'
 const pageUrls = ["/","/agenda"]
 const staticAssets = ["/styles.css","/logo-white-50.png","/whatsapp.png","/fotos/w65xrjq.jpg","/fotos/boca.jpg"]
 const networkOnlyUrls = []
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,7 +2,6 @@ import Router from './vendor/Router'
 
 const routes = new Router()
 
-routes.get('schedules', 'SchedulesController.index')
 routes.post('schedules', 'SchedulesController.create')
 routes.get('schedules/next', 'SchedulesController.next')
 routes.delete('schedules/:id', 'SchedulesController.delete')
