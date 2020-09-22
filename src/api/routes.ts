2a90e807d322461c238f585f3865afb523dfcca9
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-09-22 02:13:58 -0300

    add edit method

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,8 +3,8 @@ import Router from './vendor/Router'
 const routes = new Router()
 
 routes.post('schedules', 'SchedulesController.create')
-routes.put('schedules', 'SchedulesController.edit')
 routes.get('schedules/next', 'SchedulesController.next')
+routes.put('schedules/:id', 'SchedulesController.edit')
 routes.delete('schedules/:id', 'SchedulesController.delete')
 
 routes.get('testimonials', 'TestimonialsController.index')
