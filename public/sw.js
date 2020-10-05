cb59f464cbd035cdf11bac8be470e46647d52620
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-10-05 08:59:18 -0300

    Isolating home

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'belezaem.casa-1601896746439'
+const CACHE_NAME = 'belezaem.casa-1601899161765'
 const pageUrls = ["/","/agenda"]
 const staticAssets = ["/styles.css","/logo-white-50.png","/whatsapp.png","/fotos/w65xrjq.jpg","/fotos/boca.jpg"]
 const networkOnlyUrls = []
